c0d6f2c632135a64cd0f5863d2eebdf939f1cddf
RoyerV <varias.royer.882@gmail.com>
2023-10-25 16:22:18 -0400

    adicion de archivos faltantes de proyecto de ventas

---
+++ vistas/comprasfecha.php
@@ -0,0 +1,55 @@
+<?php
+//activamos almacenamiento en el buffer
+ob_start();
+session_start();
+if (!isset($_SESSION['nombre'])) {
+  header("Location: login.html");
+}else{
+
+
+require 'header.php';
+
+if ($_SESSION['consultac']==1) {
+
+ ?>
+    <div class="content-wrapper">
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="row">
+        <div class="col-md-12">
+      <div class="box">
+<div class="box-header with-border">
+  <h1 class="box-title">Consulta de Compras por Fecha</h1>
+  <div class="box-tools pull-right">
+    
+  </div>
+</div>
+<!--box-header-->
+<!--centro-->
+
+
+<!--fin centro-->
+      </div>
+      </div>
+      </div>
+      <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+<?php 
+}else{
+ require 'noacceso.php'; 
+}
+
+require 'footer.php';
+ ?>
+ <script src="scripts/comprasfecha.js"></script>
+ <?php 
+}
+
+ob_end_flush();
+  ?>
+
